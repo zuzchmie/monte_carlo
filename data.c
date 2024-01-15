5d09bacf84100542577d729ce7c4a03870b526ab
Zuzanna Chmielecka <zuzanna.chmielecka@protonmail.com>
2024-01-15 11:02:34 +0100

    dodanie wpisywanie uzyskanych danych do pliku data.txt

---
+++ data.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "data.h"
+
+void writePolynomialToFile(int st, int tab[],int a, int b, int n, double cal1, double cal2){
+    FILE *dataFile = fopen("data.txt", "a");
+    if (dataFile != NULL) {
+        fprintf(dataFile, "Dla wielomianu:");
+         int m = st;
+        for(int i=0; i<m; i++){
+            fprintf(dataFile, "%dx^%d+",tab[i],st);
+            st--;
+        }
+        fprintf(dataFile, "%d ",tab[m]);
+        fprintf(dataFile, "w przedziale:(%d,%d) ", a, b);
+        fprintf(dataFile, "dla %d ilosci probek:\n", n);
+        fprintf(dataFile,"Monte Carlo:%f\n", cal1);
+        fprintf(dataFile,"Simpson:%f\n", cal2);
+        fclose(dataFile);
+    } else {
+        printf("Error: Unable to open data file.\n");
+        exit(EXIT_FAILURE);
+    }
+}
---
+++ data.h
@@ -0,0 +1,5 @@
+#ifndef data_h
+#define data_h
+
+void writePolynomialToFile(int st, int tab[],int a, int b, int n, double cal1, double cal2);
+#endif
