88fc335f2c6eea18ed727c2d316ca172ebee6faf
straupisz <148783174+straupisz@users.noreply.github.com>
2024-01-20 20:34:14 +0100

    Update monte_carlo.h
    zmiana z int na double

--- monte_carlo.h
+++ monte_carlo.h
@@ -1,7 +1,7 @@
 #ifndef monte_carlo_h
 #define monte_carlo_h
 
-int f(int x, int tab[], int stopien);
+double f(double x, int tab[], int stopien);
 void createTabXandTabY_monte(double** xs,double** ys,int a, int b, int n,int tab[],int stopien);
 double monte_carlo(int a,int b, double ys[], int n);
 
