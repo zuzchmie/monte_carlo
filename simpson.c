d6b7bb6b386d8f26ad04ff6cac42c15b55d548a3
Zuzanna Chmielecka <zuzanna.chmielecka@protonmail.com>
2024-01-22 14:58:29 +0100

    error message

--- simpson.c
+++ simpson.c
@@ -10,6 +10,10 @@ void createTabXandTabYSimpson(double** xs,double** ys,int a, int b, int n, int t
     (*xs) = (double*) malloc(n*sizeof(double*));
     (*ys) = (double*) malloc(n*sizeof(double*));
 
+    if (*xs == NULL || *ys == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    exit(EXIT_FAILURE);}
+    
     for(int i=0; i<=n; i++){
         double k = a + przedzial * i;
         (*xs)[i] = k;
