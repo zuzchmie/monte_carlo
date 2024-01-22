2c624ed9063ec4b341268377b3001e3743cc71b5
Zuzanna Chmielecka <zuzanna.chmielecka@protonmail.com>
2024-01-22 14:44:18 +0100

    poprawy estetyczne

--- data.c
+++ data.c
@@ -5,17 +5,18 @@
 void writePolynomialToFile(int st, int tab[],int a, int b, int n, double cal1, double cal2){
     FILE *dataFile = fopen("data.txt", "a");
     if (dataFile != NULL) {
-        fprintf(dataFile, "Dla wielomianu: ");
+        fprintf(dataFile, "Dla wielomianu:\n ");
         int m = st;
         for(int i=0; i<m; i++){
             fprintf(dataFile, "%dx^%d + ",tab[i],st);
             st--;
         }
         fprintf(dataFile, "%d ",tab[m]);
-        fprintf(dataFile, " w przedziale:(%d,%d) ", a, b); // spacja
-        fprintf(dataFile, "dla %d ilosci probek:\n", n);
+        fprintf(dataFile, " w przedziale(%d,%d) ", a, b); 
+        fprintf(dataFile, "dla %d ilosci probek:\n\n", n);
         fprintf(dataFile, "Monte Carlo:%f\n", cal1);
-        fprintf(dataFile, "Simpson:%f\n", cal2);
+        fprintf(dataFile, "Simpson:%f\n\n", cal2);
+        fprintf(dataFile, "-------------------------------------------------------------------\n");
         fclose(dataFile);
     } else {
         printf("Error: Unable to open data file.\n");
